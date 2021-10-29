d29062314bb5597c43070baf6dc16ebe7993832e
JKlerk <joeydutchman@gmail.com>
2021-10-29 19:41:52 +0200

    Removed csv file reader

--- Infrastructure.WebService/ApiContext.cs
+++ Infrastructure.WebService/ApiContext.cs
@@ -20,53 +20,52 @@ namespace Infrastructure.WebService
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Diagnose>().ToTable("diagnose").HasKey(k => k.Id);
-
-            var records = new List<Diagnose>();
-            using (var reader = new StreamReader("Vektis_diagnose.csv"))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-            {
-                csv.Read();
-                csv.ReadHeader();
-                int inc = 1;
-                while (csv.Read())
-                {
-                    var record = new Diagnose()
-                    {
-                        Id = inc,
-                        DiagnoseCode = csv.GetField<string>("Code") != "" ? csv.GetField<Int16>("Code") : null,
-                        BodyLocation = csv.GetField<string>("lichaamslocalisatie"),
-                        Pathology = csv.GetField<string>("pathologie")
-                    };
-                    inc++;
-                    records.Add(record);
-                }
-            }
-            modelBuilder.Entity<Diagnose>().HasData(records);
-            
+            // var records = new List<Diagnose>();
+            // using (var reader = new StreamReader("Vektis_diagnose.csv"))
+            // using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            // {
+            //     csv.Read();
+            //     csv.ReadHeader();
+            //     int inc = 1;
+            //     while (csv.Read())
+            //     {
+            //         var record = new Diagnose()
+            //         {
+            //             Id = inc,
+            //             DiagnoseCode = csv.GetField<string>("Code") != "" ? csv.GetField<Int16>("Code") : null,
+            //             BodyLocation = csv.GetField<string>("lichaamslocalisatie"),
+            //             Pathology = csv.GetField<string>("pathologie")
+            //         };
+            //         inc++;
+            //         records.Add(record);
+            //     }
+            // }
+            // modelBuilder.Entity<Diagnose>().HasData(records);
+            //
             modelBuilder.Entity<TreatmentType>().ToTable("treatmenttype");
             
-            var records2 = new List<TreatmentType>();
-            using (var reader = new StreamReader("Vektis_treatment.csv"))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-            {
-                csv.Read();
-                csv.ReadHeader();
-                int inc = 1;
-                while (csv.Read())
-                {
-                    var record = new TreatmentType
-                    {
-                        Id = inc,
-                        TreatmentCode = csv.GetField<string>("Waarde"),
-                        Description = csv.GetField<string>("Omschrijving"),
-                        ExplanationRequired = csv.GetField<string>("Toelichting verplicht"),
-                    };
-                    inc++;
-                    records2.Add(record);
-                }
-            }
-            
-            modelBuilder.Entity<TreatmentType>().HasData(records2);
+            // var records2 = new List<TreatmentType>();
+            // using (var reader = new StreamReader("Vektis_treatment.csv"))
+            // using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            // {
+            //     csv.Read();
+            //     csv.ReadHeader();
+            //     int inc = 1;
+            //     while (csv.Read())
+            //     {
+            //         var record = new TreatmentType
+            //         {
+            //             Id = inc,
+            //             TreatmentCode = csv.GetField<string>("Waarde"),
+            //             Description = csv.GetField<string>("Omschrijving"),
+            //             ExplanationRequired = csv.GetField<string>("Toelichting verplicht"),
+            //         };
+            //         inc++;
+            //         records2.Add(record);
+            //     }
+            // }
+            //
+            // modelBuilder.Entity<TreatmentType>().HasData(records2);
             
         }
     }
